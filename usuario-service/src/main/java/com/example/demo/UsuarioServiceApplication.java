c1ca93da9674f87da7333a40a2939c1e4bc1419f
laura17zamora <laura.zamora.cg@gmail.com>
2023-02-02 17:04:10 -0500

    eureka

--- usuario-service/src/main/java/com/example/demo/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/example/demo/UsuarioServiceApplication.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients // habiliar feignclient
 @SpringBootApplication
+@EnableEurekaClient
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
